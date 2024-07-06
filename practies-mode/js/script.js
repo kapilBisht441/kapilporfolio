cb2db1ab28c6c2a99b16736c12356de3c904ff6d
kapilBisht441 <168058149+kapilBisht441@users.noreply.github.com>
2024-07-06 22:57:31 +0530

    Add files via upload

---
+++ practies-mode/js/script.js
@@ -0,0 +1,33 @@
+var typed = new Typed(".auto-input", {
+  strings: [" KAPIL", "WEB DEVELOPER."],
+  typeSpeed: 80,
+  backspeed: 60,
+  loop: true,
+});
+
+var typed = new Typed(".auto-type", {
+  strings: [" HTML"],
+  typeSpeed: 80,
+  backspeed: 60,
+  loop: true,
+});
+var typed = new Typed(".auto-type-one", {
+  strings: [" CSS"],
+  typeSpeed: 80,
+  backspeed: 60,
+  loop: true,
+});
+var typed = new Typed(".auto-type-two", {
+  strings: [" JAVA SCRIPT"],
+  typeSpeed: 80,
+  backspeed: 60,
+  loop: true,
+});
+var typed = new Typed(".auto-type-three", {
+  strings: [" BOOTSTRAP"],
+  typeSpeed: 80,
+  backspeed: 60,
+  loop: true,
+});
+
+AOS.init({ offset: 80, duration: 1800 });
